07d386dbb229d32a7e8e1605c96c94f523af5973
e <e@encyclopediadramatica.ch>
2012-05-02 00:48:17 +0100

    Added tuple for down/up events in G15Event

---
+++ screens/StatsScreen.py
@@ -0,0 +1,23 @@
+import datetime
+import os
+import socket
+import psutil
+from src.g15_screen import G15TextScreen
+from src.g15_screen_manager import LoopingCallMixin
+
+class StatsScreen(G15TextScreen, LoopingCallMixin):
+    def __init__(self, *a, **kw):
+        G15TextScreen.__init__(self, *a, **kw)
+        LoopingCallMixin.__init__(self)
+
+        self.appendLine("%s@%s" % (os.getlogin(), socket.gethostname()), center=True)
+        for i in xrange(4):
+            self.appendLine('')
+
+    def updateScreen(self):
+        dt = datetime.datetime.now()
+        with self.context():
+            self._text[1] = dt.strftime('%A, %b %d, %Y').center(27)
+            self._text[2] = dt.strftime('%I:%M:%S %p').center(27)
+            self._text[3] = ('CPU:%4.1f%%  RAM:%6.2f%%' % (psutil.cpu_percent(), (psutil.used_phymem()-psutil.cached_phymem())/psutil.TOTAL_PHYMEM*100)).center(27)
+            self._text[4] = ('%i processes' % len(psutil.get_pid_list())).center(27)
--- src/g15_event.py
+++ src/g15_event.py
@@ -8,27 +8,48 @@ class G15Event(ProcessProtocol):
 
     CIRCLE_DOWN = (189, 1)
     CIRCLE_UP = (189, 0)
+    CIRCLE = CIRCLE_DOWN, CIRCLE_UP
+
     BUTTON_ONE_DOWN = (190, 1)
     BUTTON_ONE_UP = (190, 0)
+    BUTTON_ONE = BUTTON_ONE_DOWN, BUTTON_ONE_UP
+
     BUTTON_TWO_DOWN = (191, 1)
     BUTTON_TWO_UP = (191, 0)
+    BUTTON_TWO = BUTTON_TWO_DOWN, BUTTON_TWO_UP
+
     BUTTON_THREE_DOWN = (192, 1)
     BUTTON_THREE_UP = (192, 0)
+    BUTTON_THREE = BUTTON_THREE_DOWN, BUTTON_THREE_UP
+
     BUTTON_FOUR_DOWN = (193, 1)
     BUTTON_FOUR_UP = (193, 0)
+    BUTTON_FOUR = BUTTON_FOUR_DOWN, BUTTON_FOUR_UP
+
 
     G1_DOWN = (167, 1)
     G1_UP = (167, 0)
+    G1 = G1_DOWN, G1_UP
+
     G2_DOWN = (168, 1)
     G2_UP = (168, 0)
+    G2 = G1_DOWN, G2_UP
+
     G3_DOWN = (169, 1)
     G3_UP = (169, 0)
+    G3 = G3_DOWN, G3_UP
+
     G4_DOWN = (170, 1)
     G4_UP = (170, 0)
+    G4 = G4_DOWN, G4_UP
+
     G5_DOWN = (171, 1)
     G5_UP = (171, 0)
+    G5 = G5_DOWN, G5_UP
+
     G6_DOWN = (172, 1)
     G6_UP = (172, 0)
+    G6 = G6_DOWN, G6_UP
 
     def __init__(self):
         self.hooks = {}
