f55b5ec0dfa766eae39ce021f88e7a00a5c973b3
e <e@encyclopediadramatica.ch>
2012-05-02 02:02:44 +0100

    Added an example on how tor un the ScreenManager (work in progress)

---
+++ example-main.py
@@ -0,0 +1,10 @@
+from twisted.internet.protocol import ClientCreator
+from screens import StatsScreen,  ImageTest
+from src.g15_screen_manager import ScreenManager
+from twisted.internet import reactor
+
+protocol = ScreenManager
+screens = [ImageTest.GifTest, StatsScreen.StatsScreen, ImageTest.ImageTest]
+
+ClientCreator(reactor, protocol, '/dev/input/event4', screens).connectTCP('localhost', 15550)
+reactor.run()
