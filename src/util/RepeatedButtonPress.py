7bcd44bb941f9ad20d2b659e4340b525b0dc7dc4
e <e@encyclopediadramatica.ch>
2012-05-02 01:46:45 +0100

    Refactor util.RepeatedButtonPress

--- src/util/RepeatedButtonPress.py
+++ src/util/RepeatedButtonPress.py
@@ -5,34 +5,34 @@ class RepeatedButtonPress:
         A class that will repeat press the button when it is held down.
     """
     def __init__(self, event, callback, delay = .75, frequency = 0.20):
-        self.event = event
-        self.callback = callback
-        self.delay = delay
-        self.frequency = frequency
-        self.timeout = None
-        self.lc = task.LoopingCall(self.callCallback)
+        self._event = event
+        self._callback = callback
+        self._delay = delay
+        self._frequency = frequency
+        self._timeoutDelay = None
+        self._loopingCall = task.LoopingCall(self.callCallback)
 
     def connect(self, event):
-        event.hookEvent(self.event[0], self.keyDown)
-        event.hookEvent(self.event[1], self.keyUp)
+        event.hookEvent(self._event[0], self._on_keyDown)
+        event.hookEvent(self._event[1], self._on_KeyUp)
 
     def callCallback(self):
-        self.callback(self)
+        self._callback(self)
 
-    def keyDown(self, event):
-        if self.timeout and self.timeout.active():
-            self.timeout.cancel()
+    def _on_keyDown(self, event):
+        if self._timeoutDelay and self._timeoutDelay.active():
+            self._timeoutDelay.cancel()
 
         self.callCallback()
-        self.timeout = reactor.callLater(self.delay, self.startLc)
+        self._timeoutDelay = reactor.callLater(self._delay, self._startLoopingCall)
 
-    def keyUp(self, event):
-        if self.timeout and self.timeout.active():
-            self.timeout.cancel()
-            self.timeout = None
-        if self.lc.running:
-            self.lc.stop()
+    def _on_KeyUp(self, event):
+        if self._timeoutDelay and self._timeoutDelay.active():
+            self._timeoutDelay.cancel()
+            self._timeoutDelay = None
+        if self._loopingCall.running:
+            self._loopingCall.stop()
 
-    def startLc(self):
-        if not self.lc.running:
-            self.lc.start(self.frequency)
+    def _startLoopingCall(self):
+        if not self._loopingCall.running:
+            self._loopingCall.start(self._frequency)
