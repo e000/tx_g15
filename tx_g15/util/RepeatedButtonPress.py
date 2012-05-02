ba8ce99bb4a74f33b52ce65aab6d00b185eb9715
e <e@encyclopediadramatica.ch>
2012-05-02 03:26:13 +0100

    Lazy import reactor in RepeatedButtonPress.py

--- tx_g15/util/RepeatedButtonPress.py
+++ tx_g15/util/RepeatedButtonPress.py
@@ -1,30 +1,30 @@
-from twisted.internet import task, reactor
+from twisted.internet import task
 
 class RepeatedButtonPress:
     """
         A class that will repeat press the button when it is held down.
     """
-    def __init__(self, event, callback, delay = .75, frequency = 0.20):
+    def __init__(self, event, callback, delay = .75, frequency = 0.20, reactor = None):
         self._event = event
         self._callback = callback
         self._delay = delay
         self._frequency = frequency
         self._timeoutDelay = None
-        self._loopingCall = task.LoopingCall(self.callCallback)
+        self._loopingCall = task.LoopingCall(self._callback)
+        if not reactor:
+            from twisted.internet import reactor
+        self.reactor = reactor
 
     def connect(self, event):
         event.hookEvent(self._event[0], self._on_keyDown)
         event.hookEvent(self._event[1], self._on_KeyUp)
 
-    def callCallback(self):
-        self._callback(self)
-
     def _on_keyDown(self, event):
         if self._timeoutDelay and self._timeoutDelay.active():
             self._timeoutDelay.cancel()
 
-        self.callCallback()
-        self._timeoutDelay = reactor.callLater(self._delay, self._startLoopingCall)
+        self._callback()
+        self._timeoutDelay = self.reactor.callLater(self._delay, self._startLoopingCall)
 
     def _on_KeyUp(self, event):
         if self._timeoutDelay and self._timeoutDelay.active():
